b617ae104004db8302799d1bf662c40293271547
Dessasin_Dev <dessasin@pop-os.localdomain>
2019-07-28 09:59:29 +0300

    sekizinci günden kalanlar tekrar ediyor

---
+++ day8/main.js
@@ -0,0 +1,51 @@
+/* var a=1;
+var b=12;
+var c=2;
+
+if(a>b && a>c){
+    if(b>c){
+        console.log("a > b > c");
+        
+    }else{
+        console.log("a > c > b");
+        
+    }
+}else if(b>a && b>c){
+    if(a>c){
+        console.log("b > a > c");
+        
+    }else{
+        console.log("b > c > a");
+        
+    }
+}
+else{
+    if
+}
+ */
+
+/* 
+ let a =4;
+ var deneme = function(){
+     let b = "ben stringim"
+     a=b;
+     return a;
+ }
+
+ var x=deneme();
+ console.log(x);
+ 
+ let a =4;
+ var deneme2 = ()=>{  //        () => işareti / function() ifadesi yerine geçer arrow funcktion
+     let b = "ben stringim"
+     a=b;
+     return a;
+ }
+
+ var x=deneme();
+ console.log(x); */
+ 
+
+ var diz =[12,3,41,2,4,5]
+
+ console.log(diz.slice(2,4))
